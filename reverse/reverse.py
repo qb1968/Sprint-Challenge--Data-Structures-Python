5a0e1a85cfe5c99f11c6bd155d47aa09b5fa8d8f
qb1968 <mark.allison75@gmail.com>
2020-05-15 13:16:07 -0400

    Still tweaking but MVP

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,15 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node is None:
+            return
+        if node.next_node is None:
+            self.head = node
+            node.next_node = prev
+            return
+
+        current = node.next_node
+
+        node.next_node = prev
+
+        self.reverse_list(current, node)
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,16 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.index = 0
 
     def append(self, item):
-        pass
 
+        if len(self.data) == self.capacity:
+            self.data[self.index] = item
+        else:
+            self.data.append(item)
+        self.index = (self.index + 1) % self.capacity # this sets the index back to zero
+        
     def get(self):
-        pass
+        return self.data
